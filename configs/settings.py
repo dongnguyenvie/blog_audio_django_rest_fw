b6416d8cad91b3ff80e83fe2bc8835f92dbbbdf1
Dong Nguyen <dong.nguyen@gmail.com>
2020-04-18 16:34:13 +0700

    updated heroku

--- configs/settings.py
+++ configs/settings.py
@@ -130,5 +130,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
