84e3faa4153948041f5ae6a856aff0f504bd81ed
Dong Nguyen <dong.nguyen@gmail.com>
2020-04-18 15:20:02 +0700

    updated advise app

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from categories.models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
