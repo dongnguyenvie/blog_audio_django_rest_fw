fc8858d35a3cf0ece2f00397baa6c021926fb468
Dong Nguyen <dong.nguyen@gmail.com>
2020-04-20 23:14:43 +0700

    updated post api

--- apps/posts/api/urls.py
+++ apps/posts/api/urls.py
@@ -3,4 +3,5 @@ from posts.api.views import PostListAPIView
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name='list'),
+    url(r'^(?P<pk>[0-9]+)(?P<pks>[0-9]+)/$', PostListAPIView.as_view(), name='list-none'),
 ]
--- apps/posts/api/views.py
+++ apps/posts/api/views.py
@@ -1,9 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 from posts.models import Post
 from posts.api.serializers import PostSerializer
 
 
-class PostListAPIView(generics.ListCreateAPIView):
+class StandardResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+
+class PostListAPIView(generics.ListCreateAPIView, generics.RetrieveAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-# PostListAPIView.
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        queryset = None
+        pk = self.request.query_params.get('pk', None)
+        pks = self.request.query_params.get('pks', None)
+        if pk is not None:
+            queryset = Post.objects.filter(pk=pk)
+        elif pks is not None:
+            pks = [int(x) for x in pks.split(',')]
+            queryset = Post.objects.filter(pk__in=pks)
+        else:
+            queryset = super(PostListAPIView, self).get_queryset()
+        return queryset
