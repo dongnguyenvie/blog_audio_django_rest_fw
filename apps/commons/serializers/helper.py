011b87573a570f577602adc5661a67e218829389
Dong Nguyen <dong.nguyen@gmail.com>
2020-04-24 21:51:16 +0700

    udpated

--- apps/commons/serializers/helper.py
+++ apps/commons/serializers/helper.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ValidationError
 from rest_framework import status, permissions
-from apps.commons.validator.extension_validation import BlogValidationError
+from ..validator.extension_validation import BlogValidationError
 
 
 def get_owner_and_blog(self, validated_data):
