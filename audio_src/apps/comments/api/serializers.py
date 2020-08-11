56e45e491255e421416cb03c66386f7996291f46
Dong Nguyen <dongnguyenvie@gmail.com>
2020-08-12 01:40:01 +0700

    updated comment

--- audio_src/apps/comments/api/serializers.py
+++ audio_src/apps/comments/api/serializers.py
@@ -3,6 +3,12 @@ from audio_src.apps.comments.models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    article_thumbnail = serializers.StringRelatedField(
+        source='article.thumbnail', read_only=True)
+    article_slug = serializers.StringRelatedField(
+        source='article.slug', read_only=True)
+
     class Meta:
         model = Comment
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ['isDeleted']
